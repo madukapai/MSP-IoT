bf991631fffa2f65e03b32baf0abe7473894e796
maduka <maduka@MADUKA-NB>
2016-11-03 15:34:05 +0800

    Add Heart Beat Monitor WebJob

---
+++ JobHeartBeatProcessor/HeartBeatProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace JobHeartBeatProcessor
+{
+    using JobHeartBeatProcessor.Processors;
+    using JobHeartBeatProcessor.Models;
+
+    public class HeartBeatProcessor
+    {
+        static int intMaxCountOfDevices = int.Parse(ConfigurationManager.AppSettings["Microsoft.Azure.IoT.MaxDeviceCount"].ToString());
+        static string strIoTHubConnectionString = ConfigurationManager.ConnectionStrings["Microsoft.Azure.IoT.ConnectionString"].ToString();
+
+        public static async Task Process()
+        {
+            // 找出裝置的清單
+            DevicesProcessor devicesProcessor = new DevicesProcessor(strIoTHubConnectionString, intMaxCountOfDevices, "");
+            List<DeviceEntity> devicesList = await devicesProcessor.GetDevices();
+            Console.WriteLine("All Devices Count:" + devicesList.Count.ToString());
+
+            // 過濾出離線的裝置資料
+            List<DeviceEntity> offlineDevices = devicesList.FindAll(
+                delegate (DeviceEntity device) 
+                {
+                    return device.ConnectionState == "Disconnected" && device.State == "Enabled";
+                }
+            );
+            Console.WriteLine("Offline Devices Count:" + offlineDevices.Count.ToString());
+
+            // 取出離線的裝置客戶資料
+            for (int i = 0; i < offlineDevices.Count; i++)
+            {
+                string strDeviceId = offlineDevices[i].Id;
+                DateTime dtLastStateTime = offlineDevices[i].LastConnectionStateUpdatedTime;
+
+                Console.WriteLine($"Device [{offlineDevices[i].Id}] is offline, Last updated time :[{dtLastStateTime.ToString()}]");
+
+                /*
+                    在這邊加入通知或是告警的機制，如透過SnedGrid寄發EMail通知
+                */
+            }
+        }
+    }
+}
---
+++ JobHeartBeatProcessor/Processors/DevicesProcessor.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobHeartBeatProcessor.Processors
+{
+    using JobHeartBeatProcessor.Models;
+    using Microsoft.Azure.Devices;
+
+    class DevicesProcessor
+    {
+
+        private List<DeviceEntity> listOfDevices;
+        private RegistryManager registryManager;
+        private String iotHubConnectionString;
+        private int maxCountOfDevices;
+        private String protocolGatewayHostName;
+
+        public DevicesProcessor(string iotHubConnenctionString, int devicesCount, string protocolGatewayName)
+        {
+            this.listOfDevices = new List<DeviceEntity>();
+            this.iotHubConnectionString = iotHubConnenctionString;
+            this.maxCountOfDevices = devicesCount;
+            this.protocolGatewayHostName = protocolGatewayName;
+            this.registryManager = RegistryManager.CreateFromConnectionString(iotHubConnectionString);
+        }
+
+        public async Task<List<DeviceEntity>> GetDevices()
+        {
+            try
+            {
+                DeviceEntity deviceEntity;
+                var devices = await registryManager.GetDevicesAsync(maxCountOfDevices);
+
+                if (devices != null)
+                {
+                    foreach (var device in devices)
+                    {
+                        deviceEntity = new DeviceEntity()
+                        {
+                            Id = device.Id,
+                            ConnectionState = device.ConnectionState.ToString(),
+                            // 暫不取得每一個裝置的ConnectionString，所以先標示為註解
+                            // 若是有需要的話，取消這行的標記以及下方CreateDeviceConnectionString副程式的註解標記
+                            // ConnectionString = CreateDeviceConnectionString(device),
+                            LastActivityTime = device.LastActivityTime,
+                            LastConnectionStateUpdatedTime = device.ConnectionStateUpdatedTime,
+                            LastStateUpdatedTime = device.StatusUpdatedTime,
+                            MessageCount = device.CloudToDeviceMessageCount,
+                            State = device.Status.ToString(),
+                            SuspensionReason = device.StatusReason
+                        };
+
+                        if (device.Authentication != null &&
+                            device.Authentication.SymmetricKey != null)
+                        {
+                            deviceEntity.PrimaryKey = device.Authentication.SymmetricKey.PrimaryKey;
+                            deviceEntity.SecondaryKey = device.Authentication.SymmetricKey.SecondaryKey;
+                        }
+
+                        listOfDevices.Add(deviceEntity);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return listOfDevices;
+        }
+
+        /*
+        /// <summary>
+        /// 建立裝置連線至IoT Hub的連線字串
+        /// </summary>
+        /// <param name="device">裝置物件</param>
+        /// <returns></returns>
+        private String CreateDeviceConnectionString(Device device)
+        {
+            StringBuilder deviceConnectionString = new StringBuilder();
+
+            var hostName = String.Empty;
+            var tokenArray = iotHubConnectionString.Split(';');
+            for (int i = 0; i < tokenArray.Length; i++)
+            {
+                var keyValueArray = tokenArray[i].Split('=');
+                if (keyValueArray[0] == "HostName")
+                {
+                    hostName = tokenArray[i] + ';';
+                    break;
+                }
+            }
+
+            if (!String.IsNullOrWhiteSpace(hostName))
+            {
+                deviceConnectionString.Append(hostName);
+                deviceConnectionString.AppendFormat("DeviceId={0}", device.Id);
+
+                if (device.Authentication != null &&
+                    device.Authentication.SymmetricKey != null)
+                {
+                    deviceConnectionString.AppendFormat(";SharedAccessKey={0}", device.Authentication.SymmetricKey.PrimaryKey);
+                }
+
+                if (this.protocolGatewayHostName.Length > 0)
+                {
+                    deviceConnectionString.AppendFormat(";GatewayHostName=ssl://{0}:8883", this.protocolGatewayHostName);
+                }
+            }
+
+            return deviceConnectionString.ToString();
+        }
+        */
+    }
+}
